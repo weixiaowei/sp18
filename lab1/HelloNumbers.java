b343c32399fed3cf939366af6ad651b5bd1ce857
魏小伟 <weixw@weixiaoweideMacBook-Pro.local>
2018-03-24 16:44:21 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumber{
+  public static void main(String[] args) {
+    int sum = 0;
+    int i = 0;
+    while(i < 10) {
+      sum = sum + 1;
+      System.out.print(sum);
+      i += 1;
+    }
+    System.out.println();
+  }
+}
